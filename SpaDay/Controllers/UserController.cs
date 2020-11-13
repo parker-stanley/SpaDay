1cadaa0b4120b6a19dd0dc80ffc6ea199ff23c72
Parker Stanley <parker.stanley@SOMO-M-DC-005.local>
2020-11-12 21:28:58 -0500

    finished the minimum requirements for the studio and bonus missions

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //original attempt, requires if statement on add but could easily delete tempUser later
+        //public IActionResult SubmitAddUserForm(User newUser, string verify)
+        //{
+        //    if(newUser.Password == verify)
+        //    {
+        //        ViewBag.user = newUser;
+        //        return View("Index");
+        //    }
+        //    else
+        //    {
+        //        ViewBag.error = "Passwords must match.";
+        //        ViewBag.tempUser = newUser;
+        //        return View("Add");
+        //    }
+        //}
+
+
+        //better version that is cleaner but doesn't allow for storing and deleting user data
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.userName = newUser.Username;
+            ViewBag.email = newUser.Email;
+            ViewBag.dateJoined = newUser.DateJoined;
+
+            if (newUser.Password == verify)
+            {
+                return View("Index");
+
+            }
+            else
+            {
+                ViewBag.error = "Passwords must match";
+                return View("Add");
+            }
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace SpaDay.Models
+{
+    public class User
+    {
+        public string Username {get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateJoined { get; }
+
+        public User() { }
+
+        public User(string username, string email, string password)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            DateJoined = DateTime.Now;
+        }
+    }
+}
