05593d469359ca702d7f6086b2c30c0f1da70c37
Parker Stanley <parker.stanley@SOMO-M-DC-005.fios-router.home>
2020-11-17 21:40:09 -0500

    completed the studio for viewModels and validation

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Username must be between five and 15 characters.")]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [EmailAddress]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Password must be between six and twenty characters.")]
+        public string Email { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Your password must match!")]
+        public string VerifyPassword { get; set; }
+    }
+}
